bcff5c792a8e864fffcbfd52c7db4c28ce4615ea
abdousalam <abdousalamdiata@gmail.com>
2024-11-15 16:14:23 +0000

    Add files via upload

---
+++ qgis2web_2024_11_15-13_21_23_801063/data/LimiteZoneintervention_5.js
@@ -0,0 +1,1 @@
+var json_LimiteZoneintervention_5 = {"type":"FeatureCollection","name":"LimiteZoneintervention_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
